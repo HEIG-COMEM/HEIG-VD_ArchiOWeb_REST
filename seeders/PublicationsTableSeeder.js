9e6fbf1c0080eb3dce780100f4d01cf68864f5fd
antoineuldry <antoine.uldry@heig-vd.ch>
2024-11-29 10:58:49 +0100

    feat(seeders) : Create publications seeder

---
+++ seeders/PublicationsTableSeeder.js
@@ -0,0 +1,29 @@
+import Publication from '../models/publication.js';
+import User from '../models/user.js';
+import { faker } from '@faker-js/faker';
+
+export const seedPublications = async () => {
+    // Clear existing publications
+    await Publication.deleteMany();
+
+    // Fetch all users to associate publications with them
+    const users = await User.find();
+
+    // Create random publications
+    const publications = users.map((user) => ({
+        frontCamera: {
+            path: faker.image.url(),
+        },
+        backCamera: {
+            path: faker.image.url(),
+        },
+        user: user._id,
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.recent(),
+    }));
+
+    // Insert new publications
+    await Publication.insertMany(publications);
+
+    console.log('Publications seeded successfully');
+};
