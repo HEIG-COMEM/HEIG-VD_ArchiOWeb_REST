f878581bba6629bbd877da4b539401ce5d4429b4
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-01-15 13:10:48 +0100

    feat(documentation): disable "Try It Out" feature in Swagger UI

--- routes/documentation.js
+++ routes/documentation.js
@@ -5,9 +5,27 @@ import swaggerUi from 'swagger-ui-express';
 
 const router = express.Router();
 
+const DisableTryItOutPlugin = function () {
+    return {
+        statePlugins: {
+            spec: {
+                wrapSelectors: {
+                    allowTryItOutFor: () => () => false,
+                },
+            },
+        },
+    };
+};
+
+const options = {
+    swaggerOptions: {
+        plugins: [DisableTryItOutPlugin],
+    },
+};
+
 // Parse the OpenAPI document.
 const openApiDocument = yaml.load(fs.readFileSync('./openapi.json'));
 // Serve the Swagger UI documentation.
-router.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocument));
+router.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocument, options));
 
 export default router;
